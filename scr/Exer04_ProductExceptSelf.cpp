53f47a7d66926606bdb9e172e674548975434d22
JoezerGoncalves <joezer.goncalves@outlook.com>
2026-08-25 20:53:51 -0300

    Add product except self logic in Exer04_ProductExceptSelf
    Implement product of array except self using vectors.

--- scr/Exer04_ProductExceptSelf.cpp
+++ scr/Exer04_ProductExceptSelf.cpp
@@ -1,3 +1,38 @@
 //
 // Created by Iftm on 18/08/2026.
 //
+#include <iostream>
+#include <vector>
+
+int main() {
+    int ent;
+    std::cin >> ent;
+    
+    // Uso de std::vector para substituir os arrays dinâmicos do C
+    std::vector<int> vetor(ent);
+    std::vector<int> vetorR(ent);
+
+    int i, j;
+
+    for(i=0; i<ent; i++){
+        std::cin >> vetor[i];
+    }
+
+    int mult = 1;
+
+    for(i=0; i<ent; i++){
+        for(j=0; j<ent; j++){
+            if(i != j){
+                mult = vetor[j]*mult;
+            }
+        }
+        vetorR[i] = mult;
+        mult = 1;
+    }
+
+    for(i=0; i<ent; i++){
+        std::cout << vetorR[i] << " ";
+    }
+
+    return 0;
+}
