13cc2338c3108d31d96da38b1a128537d4aa0ad8
JoezerGoncalves <joezer.goncalves@outlook.com>
2026-08-25 20:49:43 -0300

    Refactor Exer02_MaxProfit.cpp for clarity

--- scr/Exer02_MaxProfit.cpp
+++ scr/Exer02_MaxProfit.cpp
@@ -1,3 +1,31 @@
 //
 // Created by Iftm on 18/08/2026.
 //
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int ent;
+    std::cin >> ent;
+
+    std::vector<int> price(ent);
+    int i;
+    for(i=0; i<ent; i++){
+        std::cin >> price[i];
+    }
+
+    int lucro=0;
+    int j;
+    for(i=0; i<ent; i++){
+        for (j=i+1; j<ent; j++){
+            if(price[j]-price[i]>lucro){
+                lucro = price[j]-price[i];
+            }
+        }
+    }
+
+    std::cout << "Lucro " << lucro << " ";
+
+    return 0;
+}
